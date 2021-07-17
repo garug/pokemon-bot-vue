4e0f7d9eaacfa2b7af5ba9c9f0258be79de2bbe3
Allef Frazão Santana (Garug) <allefff@gmail.com>
2021-07-17 08:43:35 -0300

    Mudança na rota raiz

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,7 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "Home",
-    component: Home,
+    component: () => import("../views/TodosUsuarios.vue"),
   },
   {
     path: "/usuarios/:id",
