ad0b1479decfd68dc5525b41f27568e180cc77a9
Allef Frazão Santana (Garug) <allefff@gmail.com>
2021-08-22 10:15:28 -0300

    Gen3

--- src/views/Usuario.vue
+++ src/views/Usuario.vue
@@ -139,7 +139,7 @@ const Usuario = defineComponent({
       done();
     }
 
-    const length = ref(251);
+    const length = ref(386);
     const isMissingVisible = ref(false);
     const showingDetail = ref<any>(false);
     const detailedPokemon = ref(undefined);
